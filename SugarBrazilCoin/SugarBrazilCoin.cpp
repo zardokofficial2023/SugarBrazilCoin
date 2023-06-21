630cd586d8c83ecb34378e2b9e32dea7fdeae269
Zardok <contynentalcompany@gmail.com>
2023-06-21 02:37:09 -0700

    Add project files.

---
+++ SugarBrazilCoin/SugarBrazilCoin.cpp
@@ -0,0 +1,12 @@
+﻿// SugarBrazilCoin.cpp : Defines the entry point for the application.
+//
+
+#include "SugarBrazilCoin.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
